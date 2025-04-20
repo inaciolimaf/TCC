58a4307c6788edc0787499a9e69adff64786a4a2
Inácio Filho <inaciofilho.lima@gmail.com>
2025-04-20 09:03:46 -0300

    fix: backend url connection from esp

--- mulher-segura/src/config.cpp
+++ mulher-segura/src/config.cpp
@@ -1,8 +1,8 @@
 #include "config.h"
 
-const char* START_STREAM_URL = "http://192.168.3.52:3000/start-stream";
-const char* UPLOAD_CHUNK_URL = "http://192.168.3.52:3000/upload-chunk";
-const char* FINISH_STREAM_URL = "http://192.168.3.52:3000/finish-stream";
+const char* START_STREAM_URL = "http://192.168.3.52:3000/api/v1/start-stream";
+const char* UPLOAD_CHUNK_URL = "http://192.168.3.52:3000/api/v1/upload-chunk";
+const char* FINISH_STREAM_URL = "http://192.168.3.52:3000/api/v1/finish-stream";
 
 const size_t CHUNK_SAMPLES = 2048 * 6;
 const size_t CHUNK_SIZE = CHUNK_SAMPLES * 2;
